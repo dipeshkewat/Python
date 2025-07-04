b018c98cddf5cf1ffadae98059781cfa3c87daae
Dipesh Kewat <dipeshkewat8530@gmail.com>
2025-07-04 16:02:03 +0530

    Add files via upload

---
+++ second_lecture.py
@@ -0,0 +1,105 @@
+# a="Dipesh Kewat"
+# # string=a[6]
+# # print(string)
+
+# # string=a[0:9]
+# # print(string)
+
+# # string=a[0:]
+# # print(string)
+
+# # string=a[:9]
+# # print(string)
+
+# # string=a[0:]
+# # print(string)
+
+# string=a[-8:-1]
+# print(string)
+
+ #find the length of string
+# name=input("Enter First name : ")
+# print(len(name))
+
+
+# line="hi i m a $$python developer\n $$$$$$$$$$$$$$$$$$$$$$$$because i love $$python"
+# print(str(line.count("$")))
+
+
+
+
+
+# conditional statements
+
+# age=input("Enter you age : ")
+# if int(age)>=18:
+#     print("you can play and watch anime")
+
+# elif int(age)==4:
+#     print("You are Gay")
+
+# else:
+#     print("you can watch cartoon and play games")
+# print("Fuck You !!!!!!!!!!!!")
+
+
+# marks=input("Enter your Marks : ")
+# if int(marks)>=90:
+#      print("Your grade is A \n Good Boyyyy")
+# elif int(marks)>=80:
+#      print("Your grade is B \n You are a Nerd")
+# elif int(marks)>=70:
+#      print("Your grade is C\n You are a Loser")
+# elif int(marks)<70:
+#      print("Your grade is D \n You are a Dick")
+
+
+
+
+
+
+
+# age=input("Enter Your Age : ")
+# if (int(age)>=18 and int(age)<=70):
+#     print("You Can drive")
+
+# else:
+#     print("You Cannot drive")   
+
+
+
+
+#find the numbers are Even or Odd
+
+# # num=input("Enter a number: ")
+# if (int(num)%2==0):
+#     print("The Number is Even")
+# else:
+#     print("The Number IS Odd")
+
+
+
+
+
+# check the biggest number FROM  3 numbers enter by the user
+
+# a=input("Enter 1st number: ")
+# b=input("Enter 2nd number: ")
+# c=input("Enter 3rd number: ")
+
+# if(a>b and a>c):
+#     print("A is Greater" , a)
+# elif(b>a and b>c):
+#     print("B is greater" , b)
+# else:
+#     print("C is greater" , c)
+
+
+
+# check the number is multiple of 7 or not
+
+# num=input("Enter a number: ")
+# if (num % 7 == 0):
+#     print("The Number is multiple of 7")
+# else:
+#     print("The Number is not multiple of 7")
