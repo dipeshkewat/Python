8a7235c733c5056730439db7b984b30c15025bce
Dipesh Kewat <dipeshkewat8530@gmail.com>
2025-07-04 15:59:55 +0530

    Add files via upload

---
+++ first_lecture.py
@@ -0,0 +1,80 @@
+# name="Personal AI Agent"
+# ModelNo=20
+# Company="Blue Box"
+# Price=2000
+# valueForMoney=True
+
+
+# print("Name:", name)
+# print("Model No:", ModelNo)
+# print("Company:", Company)
+# print("Price:", Price)
+# print("Value for Money:", valueForMoney)
+
+# print(type(name))
+# print(type(ModelNo))
+# print(type(Company))
+# print(type(Price))
+# print(type(valueForMoney))
+
+# a=12
+# b=3
+
+# print("sum :", a+b)
+# print("sub :", a-b)
+# print("mul :", a*b) 
+# print("division :",a/b)
+# print("modulus :", a%b)
+# print("exponent :", a**b)
+# print("true or false :",a==b)
+# print("true or false :",a!=b)
+# print("true or false :",a>b)
+# print("true or false :",a<b)
+# print("true or false :",a>=b)
+
+
+# a="12"
+# b=2
+# c=int(12)
+# sum = c + b
+# print(sum)
+
+# a=int("12")
+# b=2
+# sum = a + b
+# print(sum)
+
+# a="3.14"
+# print(type(a))
+
+
+# name=input("Enter your name: ")
+# print("hello", name, "how are you?")
+
+# hero=input("Enter the value :")
+# print(type(hero), hero)
+
+# hero=int(input("Enter the value :"))
+# print(type(hero), hero)
+
+
+
+# sum of two numbers from user input
+
+# num1=(input("Enter first number : "))
+# num2=(input("Enter second number : "))
+# sum = (int(num1) + int(num2))
+# print("Sum : ",sum)
+
+
+# area=float(input("Enter the Side : "))
+# print("Area of Square : ", area*area)
+
+# num1=float(input("Enter the first number : "))
+# num2=float(input("Enter the second number : "))
+# print("Average : ",(num1+num2)/2)
+
+# a=float(input("Enter the first number : "))
+# b=float(input("Enter the second number : "))
+
+# print(a>=b)
